24d5b99ef99b5a3e817ecbf323ddedef9f0c2a23
martin-carril <martinflyvola2@gmail.com>
2026-05-13 21:44:04 -0300

    Landing page creada!! 👍

--- src/views/index.php
+++ src/views/index.php
@@ -1,15 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="es">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PDI | Slim Template 2026</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="bootstrap.min.css">
+  <title>Emprendi</title>
+  <style>
+  </style>
 </head>
-
 <body>
-  <h1>✅ 200 OK</h1>
-  <p>Si ves esto, el servidor de Slim y el motor de plantillas estan funcionando correctamente.</p>
-</body>
 
+    <nav class="navbar bg-dark">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1"><img src="./img/emprendi.png" width="100px" height="50px"></span>
+            <span class="navbar-brand mb-0 h1">|</span>
+            <span class="navbar-brand mb-0 h1">Quienes Somos</span>
+            <span class="navbar-brand mb-0 h1">|</span>
+            <span class="navbar-brand mb-0 h1">Cómo conectamos negocios</a></span>
+            <span class="navbar-brand mb-0 h1">|</span>
+            <span class="navbar-brand mb-0 h1">¿Dónde entra usted?</span>
+            <span class="navbar-brand mb-0 h1">|</span>
+            <span class="navbar-brand mb-0 h1">Empiece a comerciar!</span>
+        </div>
+    </nav>
+    <h1>Hola somos emprendi</h1>
+    <h3>Esto está bajo construcción pero es la landing page, todo funciona 👍👍👍</h3>
+</body>
 </html>
